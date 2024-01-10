23e28cb452b83d60aa09cca743077504dd882672
Bram Kuijper <bramkuijper@gmail.com>
2024-01-10 08:23:55 +0000

    testsuite nearly there

--- tests/testthat/helper-gather_simulation.R
+++ tests/testthat/helper-gather_simulation.R
@@ -126,30 +126,82 @@ make_single_file <- function(
 
 } # end make_single_file()
 
+# helper function that returns number of slashes
+# in a string (needed to sort directory depth)
+num_slashes <- function(x)
+{
+  n_slash <- gregexpr(pattern="\\/", text=x)[[1]]
+
+  # if it is not found first entry should have
+  # been -1,
+  if (n_slash[1] == -1)
+  {
+    return (0)
+  }
+
+  # it is found we should have a list with starting
+  # indices, return that list
+  return(length(n_slash))
+}
+
 # helper function to remove previously generated files during multiple tests
 remove_files <- function(
-    file_name_prefix) {
+    file_name_prefix
+    ,dir_name_prefix=NULL) {
 
   # remove all test files in their
-  all.files <- list.files(
+  all_files <- list.files(
                           path = "."
                           ,recursive = T
                           ,pattern = paste0(file_name_prefix,".*")
                           )
 
-  file.remove(all.files)
-}
+  file.remove(all_files)
+
+  # now remove directories
+  if (is.null(dir_name_prefix))
+  {
+    return
+  }
+
+  # get all dirs
+  all_dirs <- list.files(
+    path="."
+    ,recursive=T
+    ,include.dirs=T
+    ,pattern = paste0(dir_name_prefix,".*")
+  )
+
+  # now sort dirs based on number of slashes, so that we
+  # the most nested dirs first and delete them
+
+  # sort list of dirs according to slash count
+  all_dirs_sorted <- all_dirs(rev(order(num_slashes(all_dirs))))
+
+  # now go through all dirs and remove the final one
+  for (dir_i in all_dirs_sorted)
+  {
+    file.remove(all_dirs_sorted)
+  }
+} # end remove_files()
 
 # make a bunch of files of the normal type we use
 make_bunch_of_files_normal_type <- function(
-    n_files
+    n_files=10
     ,file_name_prefix = "sim_test_"
+    ,dir_name_prefix = "dir_sim_test_"
     ,envt=parent.frame()
     ,nested_dirs = F
 )
 {
+  # if nested dirs, we need a minimum of three files to put
+  # in the various directories
+  if (nested_dirs) {
+    stopifnot(n_files >= 3)
+  }
+
   # first remove any previously generated test files
-  remove_files(file_name_prefix)
+  remove_files(file_name_prefix, dir_name_prefix)
 
   # if no weird nesting just make a whole bunch of different files
   if (!nested_dirs)
@@ -165,24 +217,35 @@ make_bunch_of_files_normal_type <- function(
     }
   } else { # ok nested dir
 
+
+    n1 <- n2 <- floor(n_files/3)
+
     # we will make a few files in directory a1/
     # then a few files in directory a1/a2/
     # then a few files in directory a3/
     # and then a few in the top-level dir
-    nested_dir_name <- "test_a1/test_a2/"
+    dirs <- c("dtest_a1/test_a2/","dtest_a3",".")
 
-    dir.create(nested_dir_name, recursive=T)
-
-    for (file_i in 1:n_files)
+    for (dir_i in dirs)
     {
-      # create a file name
-      date_format = format(x=Sys.time(),"%Y_%m_%d_%H%M%S")
-      file_name = paste0(file_name_prefix,date_format,"_",file_i)
+      dir.create(nested_dir_name, recursive=T)
+
+    # create a file name
+    date_format = format(
+      x=Sys.time()
+      ,"%Y_%m_%d_%H%M%S")
+
+    for (file_i in 1:n1)
+    {
+      file_name = paste0(nested_dir_name
+                         ,file_name_prefix
+                         ,date_format
+                         ,"_",file_i)
 
       # create the file and move on
       make_single_file(file_name = file_name, envt = envt)
     }
 
-    dir.create("test_a3")
+    dir.create("dir_test_a3/")
   }
 } # make_bunch_of_files_normal_type
