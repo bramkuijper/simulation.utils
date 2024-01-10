a687efad30718fa5ef6de5b922f80296dedf7309
megabyte22 <a.l.w.kuijper@exeter.ac.uk>
2024-01-10 10:45:49 +0000

    finally getting some tests to work, yay!

--- tests/testthat/helper-gather_simulation.R
+++ tests/testthat/helper-gather_simulation.R
@@ -6,14 +6,20 @@
 # generate a bunch of parameters for a
 # putative output file that will be summarized
 # later
-generate_params <- function(delim=";",delim_two="")
+generate_params <- function(delim=";",delim_two="",n_params)
 {
+  stopifnot(n_params >= 3)
+
+  n_params_int <- n_params_float <- floor(n_params/3)
+
+  n_params_text <- n_params - n_params_int - n_params_float
+
   # generate a bunch of non-sensical
   # some parameter values
   values_parameters <- list(
-    rbinom(n=5,size=23,prob=0.5)
-    ,sample(x=c("male","female"),size=1)
-    ,rnorm(n=3)
+    rbinom(n=n_params_int,size=23,prob=0.5)
+    ,sample(x=c("male","female"),size=n_params_text, replace=T)
+    ,rnorm(n=n_params_float)
     )
 
   values_parameters <- unlist(values_parameters)
@@ -52,16 +58,17 @@ make_single_file <- function(
     file_name
     ,envt= parent.frame() # see https://testthat.r-lib.org/articles/test-fixtures.html#case-study-usethis
     ,numgen=100 # number of generations / time steps in the file
-    ,ncols=6 # number of dummy data columns
+    ,n_cols=6 # number of dummy data columns
     ,csv2=FALSE # whether we use write.csv2 and associated settings
     ,quote=FALSE # whether we quote character strings y/n
-    )
-{
+    ,delim=";" # the delimiter used to write the thing
+    ,n_params=10
+    ) {
   # set bounds on the number of columns
-  if (ncols <= 1 )  {
-    ncols <- ncols + 2 # minimum of 2
-  } else if (ncols > length(letters))  {
-    ncols <- length(letters) # max of 23
+  if (n_cols <= 1 )  {
+    n_cols <- n_cols + 2 # minimum of 2
+  } else if (n_cols > length(letters))  {
+    n_cols <- length(letters) # max of 23
   }
 
   # make 1 integer col
@@ -69,9 +76,9 @@ make_single_file <- function(
 
   # make the rest of the columns floating point
   floating.point.cols <- matrix(
-    data = rnorm(n=(ncols - 1)* numgen)
+    data = rnorm(n=(n_cols - 1)* numgen)
     ,nrow=numgen
-    ,ncol= ncols - 1)
+    ,ncol= n_cols - 1)
 
   # get the integer and floating point data
   # all together in a data.frame
@@ -93,18 +100,11 @@ make_single_file <- function(
   textconn <- textConnection(object = "tempfile"
                              ,open ="w")
 
-  if (!csv2)
-  {
-    write.csv(x=the.data
-              ,file=textconn
-              ,row.names=F
-              ,quote=F)
-  } else {
-    write.csv2(x=the.data
-              ,file=textconn
-               ,row.names=F
-              ,quote=F)
-  }
+  write.table(x=the.data
+            ,file=textconn
+            ,row.names=F
+            ,quote=quote
+            ,sep=delim)
 
   # append parameters to the string reflecting the csv
   the_csv_string <- paste0(paste0(tempfile,collapse="\n"),"\n","\n")
@@ -113,7 +113,9 @@ make_single_file <- function(
   close(textconn)
 
   # now generate the parameters
-  parameter_string <- generate_params(delim = ifelse(csv2,";",","))
+  parameter_string <- generate_params(
+    delim = delim
+    ,n_params=n_params)
 
   # generate the whole file by combining csv file
   # and the parameters
@@ -130,6 +132,11 @@ make_single_file <- function(
 # in a string (needed to sort directory depth)
 num_slashes <- function(x)
 {
+  if (length(x) == 0)
+  {
+    return(0)
+  }
+
   n_slash <- gregexpr(pattern="\\/", text=x)[[1]]
 
   # if it is not found first entry should have
@@ -147,7 +154,8 @@ num_slashes <- function(x)
 # helper function to remove previously generated files during multiple tests
 remove_files <- function(
     file_name_prefix
-    ,dir_name_prefix=NULL) {
+    ,dir_name_prefix=NULL)
+{
 
   # remove all test files in their
   all_files <- list.files(
@@ -172,11 +180,16 @@ remove_files <- function(
     ,pattern = paste0(dir_name_prefix,".*")
   )
 
+  if (length(all_dirs) <= 0)
+  {
+    return
+  }
+
   # now sort dirs based on number of slashes, so that we
   # the most nested dirs first and delete them
 
   # sort list of dirs according to slash count
-  all_dirs_sorted <- all_dirs(rev(order(num_slashes(all_dirs))))
+  all_dirs_sorted <- all_dirs[rev(order(num_slashes(all_dirs)))]
 
   # now go through all dirs and remove the final one
   for (dir_i in all_dirs_sorted)
@@ -186,12 +199,15 @@ remove_files <- function(
 } # end remove_files()
 
 # make a bunch of files of the normal type we use
-make_bunch_of_files_normal_type <- function(
+make_bunch_of_files <- function(
     n_files=10
     ,file_name_prefix = "sim_test_"
     ,dir_name_prefix = "dir_sim_test_"
     ,envt=parent.frame()
     ,nested_dirs = F
+    ,delim=";"
+    ,n_cols=10
+    ,n_params=10
 )
 {
   # if nested dirs, we need a minimum of three files to put
@@ -201,7 +217,9 @@ make_bunch_of_files_normal_type <- function(
   }
 
   # first remove any previously generated test files
-  remove_files(file_name_prefix, dir_name_prefix)
+  remove_files(
+    file_name_prefix
+    ,dir_name_prefix)
 
   # if no weird nesting just make a whole bunch of different files
   if (!nested_dirs)
@@ -213,39 +231,61 @@ make_bunch_of_files_normal_type <- function(
       file_name = paste0(file_name_prefix,date_format,"_",file_i)
 
       # create the file and move on
-      make_single_file(file_name = file_name, envt = envt)
+      make_single_file(file_name = file_name
+                       ,envt = envt
+                       ,delim=delim
+                       ,n_cols=n_cols
+                       ,n_params=n_params)
     }
   } else { # ok nested dir
 
+    # number of files per folder
+    n_files_per_folder <- c(NA,NA,NA)
 
-    n1 <- n2 <- floor(n_files/3)
+    # folders 1,2 get 1/3 of the number
+    n_files_per_folder[1] <- n_files_per_folder[2] <- floor(n_files/3)
+
+    # folder 3 gets the remainder
+    n_files_per_folder[3] <- n_files - n_files_per_folder[1] - n_files_per_folder[2]
 
     # we will make a few files in directory a1/
     # then a few files in directory a1/a2/
     # then a few files in directory a3/
     # and then a few in the top-level dir
-    dirs <- c("dtest_a1/test_a2/","dtest_a3",".")
+    dirs <- c(
+      paste0(dir_name_prefix,"_a1/",dir_name_prefix,"_a2/") # single nested dir
+      ,paste0(dir_name_prefix,"_a3/"
+      ,".")
+              ) # single non-nested dir
 
-    for (dir_i in dirs)
+    # make these directories
+    for (dir_idx in 1:length(dirs))
     {
-      dir.create(nested_dir_name, recursive=T)
+      if (dir_idx != ".")
+      {
+        dir.create(dirs[dir_idx], recursive=T)
+      }
 
-    # create a file name
-    date_format = format(
-      x=Sys.time()
-      ,"%Y_%m_%d_%H%M%S")
+      # create a file name
+      date_format = format(
+        x=Sys.time()
+        ,"%Y_%m_%d_%H%M%S")
 
-    for (file_i in 1:n1)
-    {
-      file_name = paste0(nested_dir_name
-                         ,file_name_prefix
-                         ,date_format
-                         ,"_",file_i)
+      for (file_idx in 1:n[dir_idx])
+      {
+        file_name = paste0(dirs[dir_idx]
+                           ,file_name_prefix
+                           ,date_format
+                           ,"_",file_idx
+                           ,collapse="")
 
-      # create the file and move on
-      make_single_file(file_name = file_name, envt = envt)
-    }
-
-    dir.create("dir_test_a3/")
-  }
-} # make_bunch_of_files_normal_type
+        # create the file and move on
+        make_single_file(file_name = file_name
+                         ,envt = envt
+                         ,delim=delim
+                         ,n_cols=n_cols
+                         ,n_params=n_params)
+      }
+    } # end for dir_idx
+  } # end else nested dir
+} # make_bunch_of_files
--- tests/testthat/test-gather_simulation.R
+++ tests/testthat/test-gather_simulation.R
@@ -1,3 +1,111 @@
 # test the gather_simulation functionality
 
-test_that()
+
+test_that("summarize.sims() finds correct number of files in non-nested dirs", {
+
+  n_param <- 10
+  # make a bunch of simulated files to summarize
+  make_bunch_of_files(nested_dirs=F
+    ,n_files=20
+    ,n_cols=10
+    ,n_params=n_param)
+
+  summary_data <- summarize.sims(
+    simulations_path="."
+    ,sep=";"
+    ,parameter_start_pattern="par.a"
+    ,parameter_end_pattern=NA
+    ,data_start_pattern = "a"
+    ,data_end_pattern = "^$"
+    )
+
+  expect_equal(nrow(summary_data), 20)
+})
+
+test_that("summarize.sims() finds correct number of columns in non-nested dirs", {
+
+  n_param <- 10
+  # make a bunch of simulated files to summarize
+  make_bunch_of_files(nested_dirs=F
+    ,n_files=20
+    ,n_cols=10
+    ,n_params=n_param)
+
+  summary_data <- summarize.sims(
+    simulations_path="."
+    ,sep=";"
+    ,parameter_start_pattern="par.a"
+    ,parameter_end_pattern=NA
+    ,data_start_pattern = "a"
+    ,data_end_pattern = "^$"
+    )
+
+  expect_equal(ncol(summary_data), 20)
+})
+
+test_that("summarize.sims() finds correct final row", {
+
+  n_param <- 10
+  # make a bunch of simulated files to summarize
+  make_bunch_of_files(nested_dirs=F
+    ,n_files=20
+    ,n_cols=10
+    ,n_params=n_param)
+
+  summary_data <- summarize.sims(
+    simulations_path="."
+    ,sep=";"
+    ,parameter_start_pattern="par.a"
+    ,parameter_end_pattern=NA
+    ,data_start_pattern = "a"
+    ,data_end_pattern = "^$"
+    )
+
+  # first column should be 100 time steps
+  expect_equal(summary_data[1,"a"],100)
+})
+
+test_that("summarize.sims() finds correct first parameter", {
+
+  n_param <- 10
+  # make a bunch of simulated files to summarize
+  make_bunch_of_files(nested_dirs=F
+    ,n_files=20
+    ,n_cols=10
+    ,n_params=n_param)
+
+  summary_data <- summarize.sims(
+    simulations_path="."
+    ,sep=";"
+    ,parameter_start_pattern="par.a"
+    ,parameter_end_pattern=NA
+    ,data_start_pattern = "a"
+    ,data_end_pattern = "^$"
+    )
+
+  # check whether first parameter is in there
+  expect_equal("par.a" %in% names(summary_data),T)
+})
+
+
+test_that("summarize.sims() finds correct final parameter", {
+
+  n_param <- 10
+  # make a bunch of simulated files to summarize
+  make_bunch_of_files(nested_dirs=F
+    ,n_files=20
+    ,n_cols=10
+    ,n_params=n_param)
+
+  summary_data <- summarize.sims(
+    simulations_path="."
+    ,sep=";"
+    ,parameter_start_pattern="par.a"
+    ,parameter_end_pattern=NA
+    ,data_start_pattern = "a"
+    ,data_end_pattern = "^$"
+    )
+
+  # check whether final parameter is in there
+  expect_equal(paste0("par",letters[n_param]) %in% names(summary_data),T)
+})
